823fbf0f3f47b718e76498f4eab89da02df8c359
Sava <mihajlov.sr29.2020@uns.ac.rs>
2023-04-26 00:02:43 +0200

    commit

---
+++ app/src/main/java/com/example/slagalica_application/GameOneActivity.java
@@ -0,0 +1,15 @@
+package com.example.slagalica_application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GameOneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_one);
+    }
+}
+
