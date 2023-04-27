6ca4bf54872862aa37db9b9086a7650c2667615e
cortan-SR25 <80309754+cortan-SR25@users.noreply.github.com>
2023-04-27 17:17:39 +0200

    Created activity and layout for Asocijacije game.

---
+++ app/src/main/java/com/example/slagalica_application/GameSixActivity.java
@@ -0,0 +1,14 @@
+package com.example.slagalica_application;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GameSixActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
