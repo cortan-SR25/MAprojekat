0a3fac1abc3e8e440da8cec50b4aa1c2836dd83e
Sava Mihajlov <79896945+Savaxc@users.noreply.github.com>
2023-05-02 17:54:57 +0200

    added Gui for Games

--- app/src/main/java/com/example/slagalica_application/HomeFragment.java
+++ app/src/main/java/com/example/slagalica_application/HomeFragment.java
@@ -15,18 +15,48 @@ public class HomeFragment extends Fragment {
 
     View inflatedView;
     CardView gameOneCard;
+    CardView gameTwoCard;
     CardView gameFourCard;
+    CardView gameFiveCard;
     CardView gameSixCard;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         inflatedView = inflater.inflate(R.layout.fragment_home, container, false);
         gameOneCard = inflatedView.findViewById(R.id.gameOne);
         gameOneCard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getActivity(), GameOneActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        gameTwoCard = inflatedView.findViewById(R.id.gameTwo);
+        gameTwoCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), GameTwoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        gameFourCard = inflatedView.findViewById(R.id.gameFour);
+        gameFourCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), GameFourActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        gameFiveCard = inflatedView.findViewById(R.id.gameFive);
+        gameFiveCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), GameFiveActivity.class);
                 startActivity(intent);
             }
         });
@@ -40,14 +70,7 @@ public class HomeFragment extends Fragment {
             }
         });
 
-        gameFourCard = inflatedView.findViewById(R.id.gameFour);
-        gameFourCard.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getActivity(), GameFourActivity.class);
-                startActivity(intent);
-            }
-        });
+
         return inflatedView;
 
     }
