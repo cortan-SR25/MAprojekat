63e2d7201da9a855ee21efda303290ac26fbbc65
cortan-SR25 <80309754+cortan-SR25@users.noreply.github.com>
2023-06-04 20:00:32 +0200

    Added Timer to Moj broj game

--- app/src/main/java/com/example/slagalica_application/GameOneActivity.java
+++ app/src/main/java/com/example/slagalica_application/GameOneActivity.java
@@ -3,13 +3,52 @@ package com.example.slagalica_application;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.TextView;
 
 public class GameOneActivity extends AppCompatActivity {
+    private TextView timerText;
+
+    private CountDownTimer countDownTimer;
+    private boolean isTimerRunning = false;
+
+    private long startTime = 60000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_one);
+
+        timerText = findViewById(R.id.timer);
+
+        startTimer();
+    }
+
+    private void startTimer() {
+        countDownTimer = new CountDownTimer(startTime, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                updateTimerText(millisUntilFinished);
+            }
+
+            @Override
+            public void onFinish() {
+                timerText.setText("00");
+                isTimerRunning = false;
+            }
+        };
+
+        countDownTimer.start();
+        isTimerRunning = true;
+    }
+
+    private void updateTimerText(long millisUntilFinished) {
+        int seconds = (int) (millisUntilFinished / 1000);
+
+        //seconds = seconds % 60;
+
+        String time = String.format("%02d", seconds);
+        timerText.setText(time);
     }
 }
 
--- app/src/main/java/com/example/slagalica_application/GameSixActivity.java
+++ app/src/main/java/com/example/slagalica_application/GameSixActivity.java
@@ -1,11 +1,12 @@
 package com.example.slagalica_application;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class GameSixActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
